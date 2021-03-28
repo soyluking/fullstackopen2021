1cb15c3155f6da569e5b86d8cfdcc87aa7d9c0e7
Luigi Pizarro <lpizarro@digitalmeat.cl>
2021-03-28 02:32:54 -0300

    feat: 2.1: Course information step6

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,13 @@
+import Part from './Part';
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,9 @@
+const Total = ({ course }) => {
+  const sum =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+  return <p>Number of exercises {sum}</p>;
+};
+
+export default Total;
